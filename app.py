1af0c509ddc9f1ee2df3189d01f81a555f9c8c83
Paul Rayker <paulrayker@gmail.com>
2026-06-26 16:58:08 +0200

    Added Battlelog and Evo/Hero display

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ def stats():
     headers = {"Authorization": f"Bearer {API_KEY}"}
 
     response = requests.get(
-        f"https://api.clashroyale.com/v1/players/{tag}/battlelog", headers=headers
+        f"https://api.clashroyale.com/v1/players/{tag}/battlelog",
+        headers=headers,
     )
 
     battles = response.json()
+
     wins = 0
     losses = 0
     for battle in battles:
@@ -41,10 +43,59 @@ def stats():
     player = response_player.json()
 
     for card in player["currentDeck"]:
-        deck.append({"name": card["name"], "image": card["iconUrls"]["medium"]})
+        appendings = {
+            "name": card["name"],
+        }
+        if card.get("evolutionLevel") == 2 and "heroMedium" in card["iconUrls"]:
+            appendings["image"] = card["iconUrls"]["heroMedium"]
+        elif card.get("evolutionLevel") == 1 and "evolutionMedium" in card["iconUrls"]:
+            appendings["image"] = card["iconUrls"]["evolutionMedium"]
+        else:
+            appendings["image"] = card["iconUrls"]["medium"]
+        deck.append(appendings)
 
     return render_template(
-        "stats.html", wins=wins, losses=losses, winrate=winrate, deck=deck
+        "stats.html",
+        wins=wins,
+        losses=losses,
+        winrate=winrate,
+        deck=deck,
+        tag=request.args.get("tag"),
+    )
+
+
+@app.route("/battlelog")
+def log():
+    tag = request.args.get("tag").replace("#", "%23")
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+    battle_log = requests.get(
+        f"https://api.clashroyale.com/v1/players/{tag}/battlelog",
+        headers=headers,
+    )
+    battles = battle_log.json()
+    opponents = []
+    for battle in battles:
+        current_opponent = battle["opponent"][0]
+        if current_opponent["name"] == "Morais":
+            print(current_opponent)
+        deck = []
+        for card in current_opponent["cards"]:
+            appendings = {
+                "name": card["name"],
+            }
+            if card.get("evolutionLevel") == 2 and "heroMedium" in card["iconUrls"]:
+                appendings["image"] = card["iconUrls"]["heroMedium"]
+            elif (
+                card.get("evolutionLevel") == 1
+                and "evolutionMedium" in card["iconUrls"]
+            ):
+                appendings["image"] = card["iconUrls"]["evolutionMedium"]
+            else:
+                appendings["image"] = card["iconUrls"]["medium"]
+            deck.append(appendings)
+        opponents.append({"name": current_opponent["name"], "deck": deck})
+    return render_template(
+        "battlelog.html", opponents=opponents, tag=request.args.get("tag")
     )
 
 
